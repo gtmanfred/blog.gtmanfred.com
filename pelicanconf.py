abc8a79e54dfb7ed410da6738d366efe0f368f8d
Daniel Wallace <danielwallace@gtmanfred.com>
2018-12-28 15:33:47 -0600

    update to pelican 4.x

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,21 +14,22 @@ TIMEZONE = 'America/Chicago'
 DEFAULT_LANG = 'en'
 LOCALE = 'en_US.utf8'
 
-THEME = "themes/pelican-bootstrap3"
+THEME = "themes/blueidea"
 JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
 PLUGIN_PATHS = ['plugins']
 PLUGINS = ['i18n_subsites']
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+#CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS =  (('Git Repositories', 'https://github.com/gtmanfred/'),
-          ('Feeds', '/feeds/'),
-          )
-EXTRA_PATH_METADATA={ 'extra/robots.txt': {'path': 'robots.txt'},}
+LINKS =  (
+    ('Git Repositories', 'https://github.com/gtmanfred/'),
+    ('Feeds', '/feeds/'),
+)
+EXTRA_PATH_METADATA={'extra/robots.txt': {'path': 'robots.txt'}}
 
 DEFAULT_PAGINATION = 10
 MARKUP = ('mkd', )
